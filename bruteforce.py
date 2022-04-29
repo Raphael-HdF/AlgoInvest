9fac549bc4b77ff921905b8396ddeb68a3c9fc0a
Raphael-HdF <rdefrob@gmail.com>
2022-04-29 15:01:14 +0200

    [ADD] Brute Force ok

--- bruteforce.py
+++ bruteforce.py
@@ -2,10 +2,14 @@ from pprint import pprint
 import csv
 from itertools import combinations
 from models.action import StockAction, Wallet
+from tools.timer import timer
 
 MAX_INVEST = 500
 
 path = r"brute-datas.csv"
+
+
+# path = r"short-brute-datas.csv"
 
 
 def import_datas(src):
@@ -15,14 +19,17 @@ def import_datas(src):
         return Wallet([StockAction(**row) for row in datas])
 
 
+@timer
 def brute_force():
     wallet = import_datas(path)
     best_invest = []
-    for i in range(1, len(wallet.actions)+1):
+    for i in range(1, len(wallet.actions) + 1):
         action_combinations = combinations(wallet.actions, i)
         for combination in action_combinations:
-            # if combination.cost <= MAX_INVEST:
-            best_invest.append(combination)
+            combination_wallet = Wallet(combination)
+            if combination_wallet.action_cost <= MAX_INVEST:
+                best_invest.append(combination_wallet)
+    best_invest.sort(key=lambda x: x.action_revenu, reverse=False)
     pprint(best_invest)
 
 
--- models/action.py
+++ models/action.py
@@ -12,22 +12,37 @@ class StockAction:
         except BaseException as e:
             print("On action", action, "We have an error :", e)
 
+    def __repr__(self):
+        return self.name
+
 
 class Wallet:
 
     def __init__(self, actions=[]):
-        self.action_value = 0
+        self.action_revenu = 0
+        self.action_cost = 0
         self.actions = actions
-        self.compute_actions_value()
+        self.compute_actions_values()
 
     def add_actions(self, actions):
         self.actions.append(actions)
-        self.compute_actions_value()
+        self.compute_actions_values()
 
     def remove_actions(self, actions):
         self.actions.remove(actions)
-        self.compute_actions_value()
+        self.compute_actions_values()
 
-    def compute_actions_value(self):
-        self.action_value = sum(action.cost for action in self.actions)
+    def compute_actions_values(self):
+        self.action_cost = sum(action.cost for action in self.actions)
+        self.action_revenu = sum(action.revenu for action in self.actions)
 
+    def __repr__(self):
+        result = f"-----------------------------------------\n" \
+                 f"WALLET\nRevenus:{self.action_revenu}" \
+                 f"\tCost:{self.action_cost}" \
+                 f"\tNumber of actions:{len(self.actions)}" \
+                 f"\nActions:\n"
+        for action in self.actions:
+            result += f"{action}; "
+        result += "\n-----------------------------------------\n"
+        return result
