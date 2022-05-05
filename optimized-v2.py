1b180d0f042cc487c6c9546155aec568b88ad037
Raphael-HdF <rdefrob@gmail.com>
2022-05-06 00:18:59 +0200

    [ADD] Optimizing version V2

--- optimized-v2.py
+++ optimized-v2.py
@@ -1,35 +1,35 @@
 from models.action import Wallet
 from models.import_data import Data
 from tools.timer import timer
-from math import ceil
-from sortedcontainers import SortedDict
 
 MAX_INVEST = 500.0
 
 path = r"datas/short-dataset1_Python+P7.csv"
 path = r"datas/dataset1_Python+P7.csv"
-path = r"datas/dataset2_Python+P7.csv"
+# path = r"datas/dataset2_Python+P7.csv"
 
 
 class Optimized:
 
-    @timer
     def __init__(self, path, max_invest):
         data = Data(path)
         self.wallet = data.datas
         self.max_invest = max_invest
+        print(self.launch_optimisation())
 
-        actual = [Wallet() for x in range(int(max_invest * 100) + 1)]
+    @timer
+    def launch_optimisation(self):
+        actual = [Wallet() for x in range(int(self.max_invest * 10) + 1)]
         for action in self.wallet.actions:
             memory = list(actual)
-            cost = int(action.cost * 100)
+            cost = int(round(action.cost * 10))
             for key, value in enumerate(actual[cost:], start=cost):
                 check_revenu = action.revenu + memory[key - cost].action_revenu
                 check_cost = action.cost + memory[key - cost].action_cost
                 if self.max_invest >= check_cost and check_revenu > value.action_revenu:
                     actual[key] = Wallet([action] + memory[key - cost].actions)
         best = actual[-1]
-        print(str(best))
+        return str(best)
 
 
 test = Optimized(path, MAX_INVEST)
